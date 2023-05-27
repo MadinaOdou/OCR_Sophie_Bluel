cd9643c9fc15db524f77fd23908846ef9f59152d
Madina <madinaodou@gmail.com>
2023-05-27 19:19:32 +0200

    creation new functions

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,6 +1,6 @@
-const works = fetch("http://localhost:5678/api/works")
-  .then((response) => response.json())
-  .then((works) => genererElements(works));
+// const works = fetch("http://localhost:5678/api/works")
+//   .then((response) => response.json())
+//   .then((works) => genererElements(works));
 
 function genererElements(works) {
   for (let i = 0; i < works.length; i++) {
@@ -17,7 +17,60 @@ function genererElements(works) {
   }
 }
 
-genererElements(works);
+const allBtn = document.querySelectorAll(".btn-filter");
+allBtn[0].classList.add("active");
+
+function selectBtn() {
+  const btnSelected = document.querySelectorAll(".btn-filter.active");
+  btnSelected.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  this.classList.add("active");
+}
+
+async function getWorks() {
+  const works = await fetch("http://localhost:5678/api/works");
+  const allWorks = await works.json();
+  genererElements(allWorks);
+  const objets = allWorks.filter((x) => x.category.name === "Objets");
+  const appartements = allWorks.filter(
+    (x) => x.category.name === "Appartements"
+  );
+  const hotelsRestaurants = allWorks.filter(
+    (x) => x.category.name === "Hotels & restaurants"
+  );
+
+  const boutonTous = document.querySelector(".tous");
+  boutonTous.addEventListener("click", function () {
+    document.querySelector(".gallery").innerHTML = "";
+    genererElements(allWorks);
+    boutonTous.classList.add("active");
+    selectBtn.call(boutonTous);
+  });
+
+  const boutonObjets = document.querySelector(".objets");
+  boutonObjets.addEventListener("click", function () {
+    document.querySelector(".gallery").innerHTML = "";
+    genererElements(objets);
+    selectBtn.call(boutonObjets);
+  });
+
+  const boutonAppartements = document.querySelector(".appartements");
+  boutonAppartements.addEventListener("click", function () {
+    document.querySelector(".gallery").innerHTML = "";
+    genererElements(appartements);
+    selectBtn.call(boutonAppartements);
+  });
+
+  const boutonHotelsRestaurants = document.querySelector(".hotelsRestaurants");
+  boutonHotelsRestaurants.addEventListener("click", function () {
+    document.querySelector(".gallery").innerHTML = "";
+    genererElements(hotelsRestaurants);
+    selectBtn.call(boutonHotelsRestaurants);
+  });
+}
+
+getWorks();
 
 // fetch("http://localhost:5678/api/categories")
 //   .then((response) => response.json())
@@ -25,43 +78,43 @@ genererElements(works);
 //     console.log(categories);
 //   });
 
-const boutonTous = document.querySelector(".tous");
+// const boutonTous = document.querySelector(".tous");
 
-boutonTous.addEventListener("click", function () {
-  document.querySelector(".gallery").innerHTML = "";
-  fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((works) => genererElements(works));
-});
+// boutonTous.addEventListener("click", function () {
+//   document.querySelector(".gallery").innerHTML = "";
+//   fetch("http://localhost:5678/api/works")
+//     .then((response) => response.json())
+//     .then((works) => genererElements(works));
+// });
 
-const boutonObjets = document.querySelector(".objets");
+// const boutonObjets = document.querySelector(".objets");
 
-boutonObjets.addEventListener("click", function () {
-  document.querySelector(".gallery").innerHTML = "";
-  fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((data) => data.filter((x) => x.category.name === "Objets"))
-    .then((resultat) => genererElements(resultat));
-});
+// boutonObjets.addEventListener("click", function () {
+//   document.querySelector(".gallery").innerHTML = "";
+//   fetch("http://localhost:5678/api/works")
+//     .then((response) => response.json())
+//     .then((data) => data.filter((x) => x.category.name === "Objets"))
+//     .then((resultat) => genererElements(resultat));
+// });
 
-const boutonAppartements = document.querySelector(".appartements");
+// const boutonAppartements = document.querySelector(".appartements");
 
-boutonAppartements.addEventListener("click", function () {
-  document.querySelector(".gallery").innerHTML = "";
-  fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((data) => data.filter((x) => x.category.name === "Appartements"))
-    .then((resultat) => genererElements(resultat));
-});
+// boutonAppartements.addEventListener("click", function () {
+//   document.querySelector(".gallery").innerHTML = "";
+//   fetch("http://localhost:5678/api/works")
+//     .then((response) => response.json())
+//     .then((data) => data.filter((x) => x.category.name === "Appartements"))
+//     .then((resultat) => genererElements(resultat));
+// });
 
-const boutonHotelsRestaurants = document.querySelector(".hotelsRestaurants");
+// const boutonHotelsRestaurants = document.querySelector(".hotelsRestaurants");
 
-boutonHotelsRestaurants.addEventListener("click", function () {
-  document.querySelector(".gallery").innerHTML = "";
-  fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((data) =>
-      data.filter((x) => x.category.name === "Hotels & restaurants")
-    )
-    .then((resultat) => genererElements(resultat));
-});
+// boutonHotelsRestaurants.addEventListener("click", function () {
+//   document.querySelector(".gallery").innerHTML = "";
+//   fetch("http://localhost:5678/api/works")
+//     .then((response) => response.json())
+//     .then((data) =>
+//       data.filter((x) => x.category.name === "Hotels & restaurants")
+//     )
+//     .then((resultat) => genererElements(resultat));
+// });
